83b8dc3844311e17a3f0e10d9949c73077f5329b
Camryn <camrynpatterson@yahoo.com>
2023-01-11 14:21:42 -0600

    adding scroll reveal for image, info and button elements

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function navBar() {
 }
 
 window.addEventListener('scroll', navBar)
+
+///// Scroll Reveal //////
+
+let reveal = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+
+reveal.reveal(".main-image", {delay: 400, origin: "top"});
+reveal.reveal(".main-info", {delay: 400, origin: "bottom"});
+reveal.reveal(".btn-wrapper", {delay: 400, origin: "bottom"});
